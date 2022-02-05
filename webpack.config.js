39d338a89cecf2f04db4350a2b89684987fa5b6e
Malcolm Lowery <leadappdev@gmail.com>
2022-02-05 18:37:17 -0500

    Inital commit with webpack config that includes css, jsx, html. Does support React Hot Reloading

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, './src/index.js'),
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.(css)$/,
+                exclude: /node_modules/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx', '.css']
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'styles.css'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, './dist')
+        },
+        port: 5000
+    },
+};
